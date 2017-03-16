cf86189a38e2bfa385f25c5ebfa7e8453a47df92
Jorge <pixelsjg@gmail.com>
2017-03-15 20:48:48 -0400

    Complted sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,10 @@
- import mobileMenu from './modules/mobileMenu';
- import OnScroll from './modules/onScroll';
+import mobileMenu from './modules/mobileMenu';
+import OnScroll from './modules/onScroll';
+import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
  var menu = new mobileMenu();
 
  var onScroll = new OnScroll();
+
+ var stickyHeader = new StickyHeader();
